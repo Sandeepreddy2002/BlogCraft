a775e5c5eeaf49adeb940b0f85c0f4020c60125d
Sandeeepreddy2002 <sandeepreddy149562@gmail.com>
2024-01-09 21:18:59 +0530

    notes.js file modified

--- src/notes.js
+++ src/notes.js
@@ -2,7 +2,7 @@ import { getDB,insert,saveDB } from "./db.js";
 
 
 
-export const getAllNotes notes = async ()=>{
+export const getAllNotes = async ()=>{
     const db = await getDB();
     return db.notes;
 }
